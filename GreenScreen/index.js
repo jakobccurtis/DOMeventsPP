7ba88f4ed3c5fb658bcc74308c2864a249b99bf2
John Madrid <jmadr37@gmail.com>
2022-12-14 16:45:11 -0500

    Made some changes to the js. Like Bryan, I couldn't limit it to five colors

--- GreenScreen/index.js
+++ GreenScreen/index.js
@@ -10,14 +10,14 @@ let colors = ["red", "green", "blue", "yellow", "purple"]
 let changeColorButton = document.body.querySelector('button')
 
 changeColorButton.addEventListener('click', () => {
-    chosenColor = document.body.querySelector('input').getAttribute('value');
-
-    if (chosenColor in colors) {
-        document.body.style.backgroundColor = "chosenColor"
-    }
-    else {
-        let error = document.createElement('p');
-        error.innerHTML = "You chose an invalid color";
-        document.body.appendChild;
-    }
+    let chosenColor = document.body.querySelector('#color').value
+    document.body.style.backgroundColor = chosenColor
+    // if (chosenColor in colors) {
+    //     document.body.style.backgroundColor = chosenColor;
+    // }
+    // else {
+    //     let error = document.createElement('p');
+    //     error.innerHTML = "You chose an invalid color";
+    //     document.body.appendChild;
+    // }
 })
