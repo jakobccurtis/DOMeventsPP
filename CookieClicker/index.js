aab7a55f6a04a10e43ad61b97c4be1692c65e7e9
John Madrid <jmadr37@gmail.com>
2022-12-14 15:04:00 -0500

    Made changes to the index.js

---
+++ CookieClicker/index.js
@@ -0,0 +1,12 @@
+let cookie = document.body.querySelector('img');
+
+cookie.style.height = "200px";
+
+let cookieCounter = document.body.querySelector('#cookieCounter');
+counter = 0;
+
+cookie.addEventListener('click', () => {
+    counter += 1;
+    console.log(`You've eaten ${cookieCounter} cookie`);
+    cookieCounter.innerHTML = counter;
+})
