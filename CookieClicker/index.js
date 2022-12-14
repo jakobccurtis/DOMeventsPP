89fe5bd3feba2b682fd1b6ccc05756b3a9eccec1
Jakob Curtis <jakobccurtis@gmailcom>
2022-12-14 15:05:34 -0500

    failed attempt

---
+++ CookieClicker/index.js
@@ -0,0 +1,8 @@
+let cookie = document.body.querySelector("img");
+let counter = document.body.querySelector("#counter");
+let count = 0;
+cookie.addEventListener('click', function (count) {
+    count++
+})
+
+console.log(count);
