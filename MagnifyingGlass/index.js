7f3b3682daa5aaf0b1e010cccb56878b99457ae6
John Madrid <jmadr37@gmail.com>
2022-12-14 15:30:00 -0500

    Made changes to the js

---
+++ MagnifyingGlass/index.js
@@ -0,0 +1,22 @@
+let text = document.body.querySelector('p');
+let size = 16
+
+let biggerButton = document.body.querySelector('#button1');
+
+biggerButton.addEventListener('click', () => {
+    if (size >= 1 && size <= 100) {
+        text.style.fontSize = `${size}px`;
+        size += 1;
+        console.log(size);
+    }
+})
+
+let smallerButton = document.body.querySelector('#button2');
+
+smallerButton.addEventListener('click', () => {
+    if (size >= 1 && size <= 100) {
+        text.style.fontSize = `${size}px`;
+        size -= 1;
+        console.log(size);
+    }
+})
